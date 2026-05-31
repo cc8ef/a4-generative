173c0ff6296a43b72842227dce1d4a3d135d346e
Cecilia Clucas <ceciliaeaclucas@gmail.com>
2026-05-31 16:00:06 -0400

    changed stoke saturation and brightness

--- sketches/sketch1.js
+++ sketches/sketch1.js
@@ -9,7 +9,7 @@ function setup() {
   strokeWeight(10);
 
   // Set color mode to hue-saturation-brightness (HSB)
-  colorMode(RGB);
+  colorMode(HSB);
 
   // Set screen reader accessible description
   describe('A blank canvas where the user draws by dragging the mouse');
@@ -19,7 +19,7 @@ function mouseDragged() {
   // Set the color based on the mouse position, and draw a line
   // from the previous position to the current position
   let lineHue = mouseX - mouseY;
-  stroke(lineHue, 90, 90);
+  stroke(lineHue, 45, 45);
   line(pmouseX, pmouseY, mouseX, mouseY);
 }
 
